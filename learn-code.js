6a20e27240163e91c1514ed7943b4d653124376a
sdrhr <108856459+sdrhr@users.noreply.github.com>
2022-11-19 18:10:02 +0500

    java calculator
    sum up values

---
+++ learn-code.js
@@ -0,0 +1,130 @@
+// var lenght1=14;
+// var width1=13;
+// var area1= lenght1*width1;
+
+// var lenght2=45;
+// var width2=67;
+// var area2=lenght2*width2;
+// console.log(area1);
+// console.log(area2);
+
+// function area(lenght,width)
+// {
+//     return lenght*width;
+// }
+
+// // var area1=area(10,15);
+// // console.log(area1);
+
+// var rectanglesAreas=[]
+// rectanglesAreas.push(area(12,8));
+// rectanglesAreas.push(area(34,6));
+// rectanglesAreas.push(area(12,34));
+// console.log(rectanglesAreas);
+
+// var bankBalance=500;
+// function makeTransaction(priceOfSale){
+//     if
+//         (priceOfSale<= bankBalance){
+//         bankBalance-= priceOfSale;
+//         console.log("purchase succesful");
+
+//     }else
+//      { console.log("unidendified money");
+//     }
+       
+//     }
+
+// console.log("bankBalance");
+// makeTransaction(56.98);
+// console.log("bankBalance");
+// makeTransaction(45.98);
+
+// console.log("bankBalance");
+// makeTransaction(78.98);
+
+// console.log("bankBalance");
+// makeTransaction(600.98);
+
+//  var transaction=function(priceOfSale){
+//     if
+//         (priceOfSale<= bankBalance){
+//         bankBalance-= priceOfSale;
+//         console.log("purchase succesful");
+
+//     }else
+//      { console.log("unidendified money");}
+       
+//     }
+  
+
+
+// var printCustomerName = function(first,last){
+//     console.log("First Name:" + first + " Last Name:" + Last);
+// }
+//  var applyForCreditCard=function(creditscore,soul){
+
+//  }
+//  var bankOperations=[];
+//  bankOperations.push(transaction);
+//  bankOperations.push(printCustomerName);
+//  bankOperations.push(applyForCreditCard);
+// var students=[];
+// function student(first ,last,age){
+//       this.firstName= first;
+//       this.lastName= last;
+//       this.age=  age;
+//       this.greeting= function()
+//       { return "hi, i'am "+ this.firstName +" and i am " + this.age + " years old";};
+      
+// }
+// students.push(new student("ali","tahir",8));
+// students.push(new student("faheem","tahir",8));
+// students.push(new student("cartr","hgdfd",9));
+
+// var student = students[0];
+// for(var key in student) {
+//     console.log(student[key]);
+// }
+
+// var s1=new student("ali","faheem",6);
+// console.log(s1);
+// console.log(s1.greeting());
+
+
+
+//  var student0 ={
+//      firstName:"sidra",
+//      lastName:"mehmood",
+//      age:8,
+//      greeting: function(){
+//          return "hi, i'am "+ this.firstName +" and i am " + this.age + " years old";
+//      }
+//  };
+//   console.log(student0.greeting());
+
+//  var student1=new Object();
+//  student1.firstName="sidra";
+//  student1.lastName="mehmood";
+//  student1.age= 8;
+
+//  var student2={};
+//  student2.firstName="parker";
+//  student2.lastName="john";
+//  student2.age=5;
+
+//  var students=[];
+//  students.push(student0);
+//  students.push(student1);
+//  students.push(student2);
+// //  console.log(student.firstName);
+// //  console.log(student.lastName);
+// //  console.log(student["firstNmae"]);
+// //  console.log(student["lastName"]);
+// // var studentNames=[];
+// // var studentAges=[3,5,6,7,3];
+// for( var index=0; index<students.length; index++){
+//     var student=(students[index]);
+//     console.log(student.greeting());
+// }
+
